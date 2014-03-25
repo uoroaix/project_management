6881ab3170c7ffa3a9a4223b15603b68fc219475
Alex Kuang <uoroaix@gmail.com>
2014-03-24 19:17:20 -0700

    project management first upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectManagement::Application.config.secret_key_base = '7cc35d89dc5d107cf1f7bbd71b7e2c87791475931c4548b218e9738e24a5d3ae88fdad93ad8ce35e3fa2aa48f751eee37601d5be83c579f5d822b89e57894b99'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectManagement::Application.config.session_store :cookie_store, key: '_project_management_session'
